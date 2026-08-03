5a135dfaf0119fa24ac527aa6154b53a701490e3
Kriyetive <code@kriyetive.com>
2026-08-03 13:13:08 +0530

    added frontend url for cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,12 +12,8 @@ app = FastAPI(title="Online Book Store API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://online-booking-store.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
