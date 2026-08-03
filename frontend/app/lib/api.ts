b1c2283f2d50ab4168e81b89dfe7ff48250603f6
Kriyetive <code@kriyetive.com>
2026-08-03 13:22:35 +0530

    added frontend url for cors

--- frontend/app/lib/api.ts
+++ frontend/app/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
- baseURL: "https://online-book-store-api.onrender.com",
+ baseURL: "https://online-book-store-c7g4.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
