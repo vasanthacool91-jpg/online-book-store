25b3fab6a7fc6911aa0bad200660de41ebd35de5
Kriyetive <code@kriyetive.com>
2026-08-03 11:41:03 +0530

    added frontend url for cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://online-book-store-c7g4.onrender.com"
+        "https://online-booking-store.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
