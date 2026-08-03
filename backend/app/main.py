0bba76429e588061d5fb132456a5ca2b986a5e85
Kriyetive <code@kriyetive.com>
2026-08-03 11:35:34 +0530

    added frontend url for cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "https://online-book-store-c7g4.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
